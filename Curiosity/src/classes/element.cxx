7b6c48db8d976f4ef761e8463387750f15de3dda
Daniel Vargas <dvargasp@javeriana.edu.co>
2023-04-07 18:14:04 -0500

    add vector to Element

--- Curiosity/src/classes/element.cxx
+++ Curiosity/src/classes/element.cxx
@@ -1,9 +1,10 @@
 #include "element.hpp"
-#include <string>
 
-Element::Element(const std::string& t, double s, std::string& mu, double x, double y) : type(t), size(s), measurement_unit(mu), x(x), y(y) {}
+Element::Element(const std::string& t, double s, std::string& mu, const Vector& p) : type(t), size(s), measurement_unit(mu), position(p) {}
 std::string Element::getType() const { return type; }
 double Element::getSize() const { return size; }
 std::string Element::getMeasurementUnit() const { return measurement_unit; }
-double Element::getX() const { return x; }
-double Element::getY() const { return y; }
+Vector Element::getPosition() const { return position; }
+void Element::setPosition(const Vector& position) { this->position = position; }
+//double Element::getX() const { return x; }
+//double Element::getY() const { return y; }
--- Curiosity/src/classes/element.hpp
+++ Curiosity/src/classes/element.hpp
@@ -1,22 +1,22 @@
-#ifndef ELEMENT_H
-#define ELEMENT_H
+#ifndef ELEMENT_HPP
+#define ELEMENT_HPP
 
+#include "vector.hpp"
 #include <string>
 
 class Element {
     public:
-        Element(const std::string&, double, std::string&, double, double);
+        Element(const std::string&, double, std::string&, const Vector&);
         std::string getType() const;
         double getSize() const;
         std::string getMeasurementUnit() const;
-        double getX() const;
-        double getY() const;
+        Vector getPosition() const;
+        void setPosition(const Vector&);
     private:
         std::string type;
         double size;
         std::string measurement_unit;
-        double x;
-        double y;
+        Vector position;
 };
 
 #endif

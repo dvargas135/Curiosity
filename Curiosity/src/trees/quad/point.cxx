2570428d24356eebf253747dcefd8aed7ac673fc
Daniel Vargas <dvargasp@javeriana.edu.co>
2023-04-10 19:59:31 -0500

    .

--- Curiosity/src/trees/quad/point.cxx
+++ Curiosity/src/trees/quad/point.cxx
@@ -1,5 +1,10 @@
 #include "point.hpp"
 
+double Point::getX() const { return x; }
+double Point::getY() const { return y; }
+void Point::setX(double x) { this->x = x; }
+void Point::setY(double y) { this->y = y; }
+
 Point& Point::operator = (const Point &p) {
     x = p.x;
     y = p.y;
--- Curiosity/src/trees/quad/point.hpp
+++ Curiosity/src/trees/quad/point.hpp
@@ -1,12 +1,18 @@
 #ifndef POINT_HPP
 #define POINT_HPP
 
+#include "../../classes/element.hpp"
 #include <iostream>
 
 class Point {
 public:
     double x;
     double y;
+    double getX() const;
+    double getY() const;
+    void setX(double x);
+    void setY(double y);
+    //Element e;
     Point& operator = (const Point &p);
     bool operator == (const Point &p) const;
     friend std::ostream& operator << (std::ostream &o, const Point &p);
